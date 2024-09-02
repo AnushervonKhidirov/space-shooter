c5fdad0cdcd5127fb09a6df5603c65d0f9889927
AnushervonKhidirov <ankhid98@gmail.com>
2024-09-02 21:48:30 +0500

    ship moving fixed and shooting animation added

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,31 @@ class SpaceShooter {
     canvas: HTMLCanvasElement
     context: CanvasRenderingContext2D
 
-    // key
-    keyPressed = false
+    // keys
+    // keyPressed = false
+    keyPressed: { [key: string]: boolean }
 
     // ship
     ship: HTMLImageElement
     shipPosition: TCoordinate
+    shootingInterval: any
     shipSize = 50
+    shipSpeed = 5
     shipMoveSize = 0
+
+    // bullets
+    bulletSpeed = 10
+    bullets: TCoordinate[]
+    shouldChangeBulletSide: boolean
 
     constructor(gameWrapper: Element) {
         this.gameWrapper = gameWrapper
         this.canvas = document.createElement('canvas')
         this.context = this.canvas.getContext('2d')!
+
+        this.keyPressed = {}
+        this.bullets = []
+        this.shouldChangeBulletSide = false
 
         this.ship = new Image()
         this.shipPosition = {
@@ -63,25 +75,25 @@ class SpaceShooter {
 
     drawShip() {
         this.movingShip()
-        this.context.save()
-        this.context.drawImage(this.ship, this.shipPosition.x, this.shipPosition.y, this.shipSize, this.shipSize)
+        if (this.bullets.length > 0) this.sooting()
 
-        this.context.restore()
+        this.context.drawImage(this.ship, this.shipPosition.x, this.shipPosition.y, this.shipSize, this.shipSize)
     }
 
     addShipController() {
         window.addEventListener('keydown', (e: KeyboardEvent) => {
-            if (this.keyPressed) return
-            this.keyPressed = true
+            if (e.code === 'ArrowLeft' && !this.keyPressed['ArrowLeft']) this.moveLeft()
+            if (e.code === 'ArrowRight' && !this.keyPressed['ArrowRight']) this.moveRight()
+            if (e.code === 'Space' && !this.keyPressed['Space']) this.shoot()
 
-            if (e.key === 'ArrowLeft') this.shipMoveSize = -10
-            if (e.key === 'ArrowRight') this.shipMoveSize = 10
+            this.keyPressed[e.code] = true
         })
 
         window.addEventListener('keyup', (e: KeyboardEvent) => {
-            this.keyPressed = false
-
-            this.shipMoveSize = 0
+            this.keyPressed[e.code] = false
+            
+            if (!this.keyPressed['ArrowLeft'] && !this.keyPressed['ArrowRight']) this.shipMoveSize = 0
+            if (!this.keyPressed['Space']) clearInterval(this.shootingInterval)
         })
     }
 
@@ -94,6 +106,49 @@ class SpaceShooter {
             x: fixed,
             y: this.shipPosition.y,
         }
+    }
+
+    shoot() {
+        this.shootingInterval = setInterval(() => {
+            const bulletPosition = {
+                x: this.shipPosition.x + (this.shipSize - 3) * Number(this.shouldChangeBulletSide),
+                y: this.shipPosition.y,
+            }
+            this.bullets.push(bulletPosition)
+            this.shouldChangeBulletSide = !this.shouldChangeBulletSide
+        }, 50)
+    }
+
+    sooting() {
+        this.bullets = this.bullets.map(bullet => ({
+            ...bullet,
+            y: bullet.y - this.bulletSpeed,
+        }))
+
+        this.bullets.forEach((bullet, index) => {
+            if (bullet.y < 0) {
+                this.bullets.splice(index, index + 1)
+            }
+        })
+
+        this.renderBullet()
+    }
+
+    renderBullet() {
+        this.bullets.forEach(bullet => {
+            this.context.save()
+            this.context.fillStyle = '#ffd600'
+            this.context.fillRect(bullet.x, bullet.y, this.shipSize / 15, this.shipSize / 2.5)
+            this.context.restore()
+        })
+    }
+
+    moveLeft() {
+        this.shipMoveSize = -this.shipSpeed
+    }
+
+    moveRight() {
+        this.shipMoveSize = this.shipSpeed
     }
 
     fixShipCoordinate(position: number, max: number) {
